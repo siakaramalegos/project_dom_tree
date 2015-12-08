98e30578c358141e65587dfe132346dbaae0e0a5
Sia <siakaramalegos@gmail.com>
2015-12-08 16:30:13 -0600

    Adds ability to search descendants

--- tree_searcher.rb
+++ tree_searcher.rb
@@ -9,37 +9,48 @@ class TreeSearcher
 
   # Search for and return a collection with all nodes exactly matching the name, text, id, or class
   def search_by(attribute, value)
-    @search_by_results = []
+    @search_results = []
     # search_params = get_search_params(attribute, value)
-    @search_by_results = get_search_by(attribute, value, @tree)
+    @search_results = get_search_by(attribute, value, @tree)
 
-    if @search_by_results.empty?
+    if @search_results.empty?
       puts "I'm sorry, didn't find anything with #{attribute} equal to #{value}."
     end
 
-    @search_by_results
+    @search_results
+  end
+
+  def search_descendants(node, attribute, value)
+    @search_results = []
+    @search_results = get_search_by(attribute, value, node)
+    if @search_results.empty?
+      puts "I'm sorry, didn't find anything with #{attribute} equal to #{value}."
+    end
+    @search_results
   end
 
   private
 
   def get_search_by(attribute, value, node)
     # If node is match, put it in the results
-    if (attribute == :classes || attribute == :text) && node[attribute]
-      @search_by_results << node if node[attribute].include?(value)
-    else
-      @search_by_results << node if node[attribute] == value
+    if node[attribute]
+      if attribute == :classes || attribute == :text
+        @search_results << node if node[attribute].include?(value)
+      else
+        @search_results << node if node[attribute] == value
+      end
     end
 
     # Look at children
     if node.children.nil? || node.children.empty?
-      return @search_by_results
+      return @search_results
     else
       node.children.each do |child|
         get_search_by(attribute, value, child)
       end
     end
 
-    @search_by_results
+    @search_results
   end
 end
 
@@ -47,7 +58,10 @@ reader = DOMReader.new
 tree = reader.build_tree("test.html")
 renderer = NodeRenderer.new(tree)
 searcher = TreeSearcher.new(tree)
-searcher.search_by(:classes, "sidebar").each { |node| renderer.render(node) }
-searcher.search_by(:classes, "bold").each { |node| renderer.render(node) }
-searcher.search_by(:tag, "div").each { |node| renderer.render(node) }
-searcher.search_by(:text, "div").each { |node| renderer.render(node) }
+# searcher.search_by(:classes, "sidebar").each { |node| renderer.render(node) }
+# searcher.search_by(:tag, "div").each { |node| renderer.render(node) }
+# searcher.search_by(:text, "div").each { |node| renderer.render(node) }
+my_node = searcher.search_by(:id, "main-area")
+my_node.each { |node| renderer.render(node) }
+descendants = searcher.search_descendants(my_node[0], :tag, "li")
+descendants.each { |node| renderer.render(node) }
