823f8067e31b01042c15b24c79fa76412bdd69aa
Sia <siakaramalegos@gmail.com>
2015-12-08 17:06:54 -0600

    Adds ability to build html file from the node tree

--- node_renderer.rb
+++ node_renderer.rb
@@ -27,7 +27,51 @@ class NodeRenderer
     end
   end
 
+  def build_file(file_name)
+    @lines = []
+    html_lines = get_html_lines(@tree, 0)
+
+    File.open(file_name, 'w') do |file|
+      file.write html_lines.join("\n")
+    end
+
+    puts "Saved successfully!"
+  end
+
   private
+
+  def get_html_lines(node, depth)
+    @lines << parse_node(node, depth)
+    if node.children.nil? || node.children.empty?
+      return @lines
+    else
+      node.children.each do |child|
+        get_html_lines(child, depth + 1)
+      end
+      @lines << "#{' ' * 2 * depth}</#{node.tag}>"
+    end
+    @lines
+  end
+
+  def parse_node(node, depth)
+    spaces = ' ' * 2 * depth
+    if node.text
+      spaces + node.text
+    else
+      buffer = ''
+
+      if node.classes
+        classes = " class='#{node.classes.join(' ')}'"
+        buffer = ' ' if node.id
+      else
+        classes = ''
+      end
+
+      node.id ? id = " id='#{node.id}'" : id = ''
+
+      "#{spaces}<#{node.tag}#{id}#{buffer}#{classes}>"
+    end
+  end
 
   def get_size_by_type(node)
     if node.children.nil? || node.children.empty?
@@ -47,7 +91,8 @@ class NodeRenderer
   end
 end
 
-# reader = DOMReader.new
-# tree = reader.build_tree("test.html")
-# renderer = NodeRenderer.new(tree)
-# renderer.render()
+reader = DOMReader.new
+tree = reader.build_tree("test.html")
+renderer = NodeRenderer.new(tree)
+renderer.render()
+renderer.build_file("build.html")
